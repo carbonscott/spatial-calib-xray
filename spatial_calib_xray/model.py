9ecb8ad72bd6660bcbfdae4beaec73a82fc6b8c2
carbonscott <wangimagine@gmail.com>
2022-05-10 16:34:42 -0700

    Provide docstring.

--- spatial_calib_xray/model.py
+++ spatial_calib_xray/model.py
@@ -106,44 +106,46 @@ class OptimizeCircleModel(CircleModel):
 
 
 class ConcentricCircles:
+
     def __init__(self, cx, cy, r, num = 100):
         super().__init__()
 
-        self.cx  = cx
-        self.cy  = cy
-        self.r   = np.array([r]).reshape(-1)    # Expect dim = (N,)
-        self.num = num
-        self.crds = np.zeros((2, num * len(self.r)))    # 2 is the size of (x, y)
-
-
-    def set_seed(self, seed):
-        np.random.seed(seed)
+        self.cx  = cx                                # Beam center position in pixels along x-axis (axis = 1 in numpy format)
+        self.cy  = cy                                # Beam center position in pixels along y-axis
+        self.r   = np.array([r]).reshape(-1)         # List of radii for all concentric circles in pixels
+        self.num = num                               # Number of pixels sampled from a circle
+        self.crds = np.zeros((2, num * len(self.r))) # Coordinates where pixels are sampled from all circles.  Unit is pixel.  2 is the size of (x, y)
 
 
     def generate_crds(self):
-        theta = np.linspace(0.0, 2 * np.pi, self.num)
-
+        """
+        Generate coordinates of sample points along each concentric circle
+        """
+        # Fetching initial values that define concentric circles...
         cx = self.cx
         cy = self.cy
-        r  = np.array(self.r).reshape(-1, 1)    # Facilitate broadcasting in calculting crds_x and crds_y
+        r  = np.array([self.r]).reshape(-1, 1) # Facilitate broadcasting in calculting crds_x and crds_y
 
-        # Convert input to numpy array with shape (N,)...
+        # Find all theta values for generating coordinates of sample points...
+        theta = np.linspace(0.0, 2 * np.pi, self.num)
         if not isinstance(theta, np.ndarray): theta = np.array([theta]).reshape(-1)
 
-        len_theta = len(theta)
-
+        # Generate coordinates...
         crds_x = r * np.cos(theta) + cx
         crds_y = r * np.sin(theta) + cy
 
-        # Reshape crds into one flat array...
-        self.crds[1] = crds_x.reshape(-1)   # In image, horizontal axis is axis=1 in matrix
+        # Reshape crds into one flat array to facilitate optimization routine...
+        self.crds[1] = crds_x.reshape(-1)
         self.crds[0] = crds_y.reshape(-1)
 
         return None
 
 
     def get_pixel_values(self, img):
-        ## pvals = map_coordinates(img, self.crds, order=1, mode='nearest')
+        """
+        Get pixel values from all sample points.  If a sample point has 
+        subpixel coordinates, interpolation will take place.  
+        """
         pvals = map_coordinates(img, self.crds)
 
         return pvals
@@ -152,9 +154,11 @@ class ConcentricCircles:
 
 
 class OptimizeConcentricCircles(ConcentricCircles):
+
     def __init__(self, cx, cy, r, num):
         super().__init__(cx, cy, r, num)
 
+        # Provide parameters for optimization...
         self.params = self.init_params()
         self.params.add("cx", value = cx)
         self.params.add("cy", value = cy)
@@ -163,13 +167,24 @@ class OptimizeConcentricCircles(ConcentricCircles):
         for i in range(len(r)): self.params.add(f"r{i:d}" , value = r[i] )
 
 
-    def init_params(self): return lmfit.Parameters()
+    def init_params(self): 
+        """
+        Initialize parameters for optimization.
+        """
+        return lmfit.Parameters()
 
 
-    def unpack_params(self, params): return [ v.value  for _, v in params.items() ]
+    def unpack_params(self, params): 
+        """
+        Unpack all parameters from dictionary.
+        """
+        return [ v.value  for _, v in params.items() ]
 
 
     def residual_model(self, params, img, **kwargs):
+        """
+        Calculate the residual for least square optimization.
+        """
         parvals = self.unpack_params(params)
         self.cx, self.cy = parvals[:2]
         self.r = parvals[2:]
@@ -184,6 +199,9 @@ class OptimizeConcentricCircles(ConcentricCircles):
 
 
     def fit(self, img, **kwargs):
+        """
+        Fit the residual model.
+        """
         print(f"___/ Fitting \___")
         res = lmfit.minimize( self.residual_model,
                               self.params,
@@ -196,6 +214,9 @@ class OptimizeConcentricCircles(ConcentricCircles):
 
 
     def report_fit(self, res):
+        """
+        Report details of the optimization.  
+        """
         lmfit.report_fit(res)
 
 
